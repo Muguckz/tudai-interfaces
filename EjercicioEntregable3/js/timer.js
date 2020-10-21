53ccf5ec12b42110f2cc67c88730f54f2db00793
Muguckz <fedemuguruza@gmail.com>
2020-10-20 23:34:29 -0300

    Se movieron cosas de lugar

---
+++ EjercicioEntregable3/js/timer.js
@@ -0,0 +1,57 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", () => {
+
+	timer();
+	window.addEventListener("scroll", animacionScroll);
+
+})
+
+function timer() {
+	let estreno = new Date("Nov 31, 2020 18:00:00").getTime();
+
+	let ciclo = setInterval(function() {
+		let tiempoAhora = new Date().getTime();
+
+		let distancia = estreno - tiempoAhora;
+		let dias = Math.floor(distancia/(1000 * 60 * 60 * 24));
+		let horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		let minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
+		let segundos = Math.floor((distancia % (1000 * 60)) / 1000);
+
+		document.querySelector("#estreno").innerHTML = "Faltan: " + dias + " días, " + horas + " horas, " + minutos + " minutos, " + segundos + " segundos.";
+
+	}, 1000);
+}
+
+function animacionScroll() {
+
+	/* Animación timer */
+
+	// let timer = document.querySelector(".timer");
+
+	// if (window.scrollY >= 1000) {
+	// 	timer.classList.add("opacidad");
+	// } else {
+	// 	timer.classList.remove("opacidad");
+	// }
+
+	/* Animación cards */
+
+	let cards = document.querySelector(".cards");
+
+	if (window.scrollY >= 1200) {
+		cards.classList.add("opacidad");
+	} else {
+		cards.classList.remove("opacidad");
+	}
+
+	let slider = document.querySelector(".slider-container");
+
+	if (window.scrollY >= 1800) {
+		slider.classList.add("opacidad");
+	} else {
+		slider.classList.remove("opacidad");
+	}
+
+}
